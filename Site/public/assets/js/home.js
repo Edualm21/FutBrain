f679b22fbc07816bbd219256c925865cec52aec6
Eduardo <eduardo.mendes@sptech.school>
2025-04-27 15:58:30 -0300

    Esqueleto home dashboards e quizzes

---
+++ Site/public/assets/js/home.js
@@ -0,0 +1,48 @@
+function premier(){
+    window.location = '../dashboard/premier.html'    
+}
+function laliga(){
+    window.location = '../dashboard/laliga.html'    
+}
+function serieA(){
+    window.location = '../dashboard/serieA_tim.html'    
+}
+function bundesliga(){
+    window.location = '../dashboard/bundesliga.html'    
+}
+function brasileirao(){
+    window.location = '../dashboard/brasileirao.html'    
+}
+function champions(){
+    window.location = '../dashboard/champions.html'    
+}
+function worldCup(){
+    window.location = '../dashboard/worldCup.html'    
+}
+function futebol(){
+    window.location = '../dashboard/futebol.html'    
+}
+function premierDash(){
+    window.location = '../dashboard/'    
+}
+function laligaDash(){
+    window.location = '../dashboard/'    
+}
+function serieADash(){
+    window.location = '../dashboard/'    
+}
+function bundesligaDash(){
+    window.location = '../dashboard/'    
+}
+function brasileiraoDash(){
+    window.location = '../dashboard/'    
+}
+function championsDash(){
+    window.location = '../dashboard/'    
+}
+function worldCupDash(){
+    window.location = '../dashboard/'    
+}
+function futebolDash(){
+    window.location = '../dashboard/'    
+}
